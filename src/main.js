5ad2e45e5e2d8f407be6658d3dd370dbcab6bb40
Jan-Sebastian Nystedt <33163299+jnyste@users.noreply.github.com>
2018-10-05 08:18:56 +0000

    Add main.js stub

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = fs.readFileSync('config/token.txt', 'utf8');
+
+client.on('ready', () => {
+   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(token);
